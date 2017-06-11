bb94c79e3fc585b8973654ce6af9bec6b7dcecf3
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-06-10 17:25:16 -0700

    I feel really lost and do not know what I am doing anymore

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict'
-
-function getRandom(){
-  return Math.random();
+var names = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+function getCustomer(min, max){
+  min = Math.minCustomersPerHour(min);
+  max = Math.maxCustomersPerHour(max);
+  return Math.maxCustomersPerHour(Math.random()* (max - min)) + min;
 }
