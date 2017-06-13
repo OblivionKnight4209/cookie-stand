23cb38a1f9e614b26851b68ed757a68683ceb308
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-06-12 19:24:40 -0700

    added new javascript file updating all files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
-var names = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
-function getCustomer(min, max){
-  min = Math.minCustomersPerHour(min);
-  max = Math.maxCustomersPerHour(max);
-  return Math.maxCustomersPerHour(Math.random()* (max - min)) + min;
-}
+// var names = cookieStoreLocations ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+// function getCustomer(min, max){
+//   min = Math.minCustomersPerHour(min);
+//   max = Math.maxCustomersPerHour(max);
+//   return Math.maxCustomersPerHour(Math.random()* (max - min)) + min;
+// }
---
+++ app_tablesales.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function StoreInfo(minPrice, maxPrice, Avg){
+  this.minPrice = minPrice;
+  this.maxPrice = maxPrice;
+  this.Avg  = Avg;
+}
+
+function makeRow(item){
+  //Step 1 -creat element
+  var newRowEl = document.createElement('tr');
+
+  //Step2 - Configure/Mutate the Row element
+  var itemData
+}
