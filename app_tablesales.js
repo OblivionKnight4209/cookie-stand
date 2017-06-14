572e15ab9a720f005cfb9d72b7dd18e16acc0e6a
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-06-13 20:13:22 -0700

    Added elements to js

--- app_tablesales.js
+++ app_tablesales.js
@@ -11,5 +11,21 @@ function makeRow(item){
   var newRowEl = document.createElement('tr');
 
   //Step2 - Configure/Mutate the Row element
-  var itemData
-}
+  var itemMinPriceEl = document.createElement('td');
+  itemMinPriceEl.textContent = this.minPrice;
+  newRowEl.appendChild(itemMinPriceEl);
+
+  var itemMaxPriceEl = document.createElement('td');
+  itemMaxPriceEl.textContent = this.maxPrice;
+  newRowEl.appendChild(itemMaxPriceEl);
+
+  var itemAvgEl = document.createElement('td');
+  itemAvgEl.textContent = this.Avg;
+  newRowEl.appendChild(itemAvgEl);
+
+  //Stage-3 Append Element to form
+  tableBodyEl.appendChild(newRowEl);
+};
+var tableEl = document.getElementById('generated-table');
+var tableBodyEl = document.createElement('tbody');
+tableEl.appendChild(tableBodyEl);
